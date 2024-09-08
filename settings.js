c3360d75c7e211dd76252f33fee94f1720f8f54d
fortermu <167023239+fortermu@users.noreply.github.com>
2024-09-08 10:39:40 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=qBVQlDzD#HQBOK6ckeWL3icjar7BiR72awxlkuY0Gwg3b91MgFRQ' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=TdEn1RLC#BrmdxhUsVk8SxoU8iFIjtWL-AlTuZTzSSPtnSTQWyew' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923263429027' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
